3cc186a81b866997d1108fc19cb1afaa172ef328
matteo_lovato <lovato.teo@gmail.com>
2020-12-02 15:20:27 +0100

    can score upon passing a pipe

--- main.py
+++ main.py
@@ -38,13 +38,17 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe, pipe)
 
 def check_collision(pipes):
+    global can_score
+    
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
+            can_score = True
             return False
     # if collision with roof or floor
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
         death_sound.play()
+        can_score = True
         return False
 
     return True
@@ -59,6 +63,7 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 def score_display(game_state):
+
     # if playing display score
     if game_state == True:
         score_surface = game_font.render(f'Score: {int(score)}', True, (255,255,255))
@@ -87,6 +92,18 @@ def update_score(score, high_score):
         high_score = score
     return high_score
 
+def pipe_score_check():
+    global score, can_score
+
+    if pipe_list:
+        for pipe in pipe_list:
+            if 95 < pipe.centerx < 105 and can_score:
+                score += 1
+                score_sound.play()
+                can_score = False
+            if pipe.centerx < 0:
+                can_score = True
+
 # init the sound mixer
 # low the quality of the sound to play it instantly
 # pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
@@ -103,6 +120,8 @@ bird_movement = 0
 game_active = True
 score = 0
 high_score = 0
+# prevent the double score
+can_score = True
 
 # get image from assets
 bg_surface = pygame.image.load('assets/background-day.png').convert() #convert help pygame to work with that image
@@ -207,13 +226,9 @@ while True:
         draw_pipes(pipe_list)
 
         game_active = check_collision(pipe_list)
-        
-        score += 0.01
-        score_sound_countdown -= 1
-        if score_sound_countdown <= 0:
-            score_sound.play()
-            score_sound_countdown = 100
-        
+
+        pipe_score_check()
+
     else:
         high_score = update_score(score, high_score)
 
